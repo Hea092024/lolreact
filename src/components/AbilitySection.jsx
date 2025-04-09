03c4bdfba07633d204161aab1f6faed627d26275
Hector Acevedo <HEA092024@gmail.com>
2025-04-09 11:18:08 +0200

    edited AbilitySection

--- src/components/AbilitySection.jsx
+++ src/components/AbilitySection.jsx
@@ -1,7 +1,26 @@
-export default function AbilitySection() {
-    return (
-        <div>
-            <h1>Ability Section</h1>
-        </div>
-    );
+export default function AbilitySection({
+  id,
+  title,
+  subtitle,
+  description,
+  videoSrc,
+}) {
+  return (
+    <>
+      <section className="bg-img" id={id}>
+        <h2>{title}</h2>
+      </section>
+      <section className="information">
+        <article>
+          <div className="paragraph-container">
+            <h3>{subtitle}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="video-link">
+            <video loop muted autoPlay src={videoSrc}></video>
+          </div>
+        </article>
+      </section>
+    </>
+  );
 }
