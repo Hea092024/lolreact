ff59f65b51c4afbfd033608964ff05fe21dcb816
Hector Acevedo <HEA092024@gmail.com>
2025-04-09 11:17:26 +0200

    edited Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,46 @@
-export default function Header () {
-    return (
-<div>
-    <h1>Header</h1>
-</div>
+import { useState } from "react";
 
-    )
+export default function Header() {
+  return (
+    <header id="top">
+      <nav>
+        <h1>
+          <a href="#first">Sona - Maven of the Strings</a>
+        </h1>
+
+        <form className="dropdown-form">
+          <select
+            className="dropdown"
+            name="page"
+            onChange={(e) => {
+              if (e.target.value) window.location.href = e.target.value;
+            }}
+          >
+            <option value="">Select a Champion</option>
+            <option value="lux.html">Lux</option>
+            <option value="seraphine.html">Seraphine</option>
+            <option value="yuumi.html">Yuumi</option>
+          </select>
+        </form>
+
+        <ul>
+          <li>
+            <a href="#first">Passive</a>
+          </li>
+          <li>
+            <a href="#second">Q</a>
+          </li>
+          <li>
+            <a href="#third">W</a>
+          </li>
+          <li>
+            <a href="#fourth">E</a>
+          </li>
+          <li>
+            <a href="#fifth">R</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
