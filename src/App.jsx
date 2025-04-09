c9ae1a0e53b4dabbee2eac4df4418c3396725c91
Hector Acevedo <HEA092024@gmail.com>
2025-04-09 10:47:56 +0200

    imported the components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import HomePage from './components/HomePage'
+import AbilitySection from './components/AbilitySection'
+import Header from './components/Header'
 
 export default function App() {
 
