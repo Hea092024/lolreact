a9a9aaf1144afe5d97e6224c42f4084bafae7df5
Hector Acevedo <HEA092024@gmail.com>
2025-04-10 08:11:05 +0200

    imported Header + AbilitySection to YuumiPage.jsx

--- src/components/YuumiPage.jsx
+++ src/components/YuumiPage.jsx
@@ -1,9 +1,6 @@
-export default function YuumiPage () {
-    return (
+import Header from "./Header";
+import AbilitySection from "./AbilitySection";
 
-        <>
-        
-        
-        </>
-    )
+export default function YuumiPage() {
+  return <></>;
 }
