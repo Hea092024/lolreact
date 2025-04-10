5ce503ffcdc5cdc20a40d81bc76049ce16432a8d
Hector Acevedo <HEA092024@gmail.com>
2025-04-10 14:21:11 +0200

    edited YuumiPage

--- src/components/YuumiPage.jsx
+++ src/components/YuumiPage.jsx
@@ -2,5 +2,8 @@ import Header from "./Header";
 import AbilitySection from "./AbilitySection";
 
 export default function YuumiPage() {
-  return <></>;
+  return (
+
+    <div><h1>hei</h1></div>
+  )
 }
